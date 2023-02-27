6f8ded008294fc676b16b221b79b936e8aa2d8a1
smaitrey <shreyans.maitrey@gnail.com>
2023-02-28 00:58:08 +0530

    MyClass file

--- OOPS/MyClass.cpp
+++ OOPS/MyClass.cpp
@@ -15,12 +15,21 @@ class MyClass{
 };
 
 int main(){
-	MyClass Obj1; //Create object of MyClass
+	MyClass Obj1; //Statically Create object of MyClass
 	Obj1.myNum = 1;
 	Obj1.myString = "Hello";
 	strcpy(Obj1.name, "Shreyans");
 
 	Obj1.display();
+
+	//Dynamically create object of MyClass
+	
+	MyClass *Obj2 = new MyClass;
+	Obj2->myNum = 2;
+	Obj2->myString = "Hi";
+	strcpy(Obj2->name, "Maitrey");
+
+	Obj2->display();
 
 	return 0;
 }
