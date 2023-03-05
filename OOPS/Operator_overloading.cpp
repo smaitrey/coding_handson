77bc603eff47660dfedb0afb4a7907dd9d000248
smaitrey <shreyans.maitrey@gnail.com>
2023-03-06 00:04:13 +0530

    Operator overloading

---
+++ OOPS/Operator_overloading.cpp
@@ -0,0 +1,99 @@
+#include<iostream>
+using namespace std;
+//#include "Fraction.cpp"
+
+class Fraction{
+private:
+	int numerator;
+	int denominator;
+public:
+	Fraction(int numerator, int denominator){
+		this->numerator = numerator;
+		this->denominator = denominator;
+	}
+
+	void print(){
+		cout<<this->numerator<<" "<<this->denominator<<endl;
+	}
+	
+	// Function to simplify Fraction
+                void Simplify(){
+                        int gcd = 1;
+                        int j = min(this->numerator, this->denominator);
+                        for(int i = 2; i <= j; i++){
+                                if(this->numerator % i == 0 && this->denominator % i == 0){
+                                        gcd = i;
+                                }
+                        }
+                        this->numerator = this->numerator / gcd;
+                        this->denominator = this->denominator / gcd;
+                }
+
+	Fraction add(Fraction const &f2){
+		int lcm = this->denominator * f2.denominator;
+		int x = lcm / this->denominator;
+		int y = lcm / f2.denominator;
+		int num = x * this->numerator + (y * f2.numerator);
+
+		Fraction fNew(num, lcm);
+		fNew.Simplify();
+		return fNew;
+	}
+
+	// Overload + operator
+	//
+	Fraction operator+(Fraction const &f2){
+		int lcm = this->denominator * f2.denominator;
+                int x = lcm / this->denominator;
+                int y = lcm / f2.denominator;
+                int num = x * this->numerator + (y * f2.numerator);
+
+                Fraction fNew(num, lcm);
+                fNew.Simplify();
+                return fNew;
+	}
+
+	// Overload * operator
+	//
+	Fraction operator*(Fraction const &f2){
+		int n = this->numerator * f2.numerator;
+		int d = this->denominator * f2.denominator;
+		Fraction fNew(n, d);
+		fNew.Simplify();
+		return fNew;
+	}
+
+	// Overload == operator
+	//
+	bool operator==(Fraction const &f2) const {
+		return (numerator == f2.numerator) && (denominator == f2.denominator);
+	}
+
+
+};
+
+
+int main(){
+	Fraction f1(10,2);
+	Fraction f2(15,4);
+
+	Fraction f3 = f1.add(f2);
+	Fraction f4 = f1 + f2;
+	f1.print();
+	f2.print();
+	f3.print();
+	f4.print();
+
+	Fraction f5 = f1 * f2;
+	f5.print();
+
+	if(f1 == f2){
+		cout<<"Equal "<<endl;
+	}
+	else{
+		cout<<"Not equal"<<endl;
+	}
+
+	return 0;
+}
+
